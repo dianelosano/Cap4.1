894ebaa3e1ae09d1e222e61f846f8bdbbb10ab8d
Diane Losano <89275679+dianelosano@users.noreply.github.com>
2022-02-16 01:34:46 -0300

    Atualização função

--- exercicio.js
+++ exercicio.js
@@ -1,12 +1,12 @@
 /* Atividade 1 - Fase 4 || Diane C. Lopes Losano - RM92172  */
 
-/* Variavel contendo o HTML copiado*/
 const valorHtml = `<html><head><title>Gulliver Traveller - Roteiros</title></head><body><b>->1 - Roteiros para *São Paulo*</b><br>A Terra da Garoa!<br>Fundada em 25 de janeiro de 1554 a cidade tem hoje cerca de 12 milhões de habitantes e é considerada o centro financeiro do Brasil e aqui vão 3 dicas de roteiros obrigatórios para aqueles que passam pela capital paulista<br>#Roteiro A | Região: Avenida Paulista<br>MASP; Parque Trianon; Rua Augusta<br>#Roteiro B | Região: Centro<br>Catedral da Sé; Pátio do Colégio; Rua Augusta<br>#Roteiro C | Região: Vila Madalena<br>Beco do Batman; Feirinha da Benedito Calixto; Livraria da Vila<br> <b>->2 - Roteiros para *Las Vegas*</b><br>Viva Las Vegas!<br>       A cidade mais populosa e mais densamente povoada do estado de Nevada, Las Vegas foi fundada em 1905 e é considerada uma cidade, oficialmente, desde 1911 e conta com mais de meio milhão de habitantes. Venha conhecer a capital dos jogos de azar!<br>#Roteiro A | Região: Las Vegas Boulevard South<br>Fonte do Bellagio; Principais Cassinos; Madame Tussauds<br>#Roteiro B | Região: Downtown<br>; Fremont; Las Vegas Art Museum; Museu nacional do Crime Organizado; <br>#Roteiro C | Região: Las Vegas Boulevard North<br>Outlet Premium North; Stratosphere; Apple Fashion Show<br><b>->3 - Roteiros para *Moscou*</b><br>Privet!<br>A capital Russa fica situada às margens do Rio Moscou e apesar de ser a cidade mais cosmopolita da Rússia, conta com grande resguardo de sua história soviética<br>#Roteiro A | Região: Praça Vermelha<br>Museu Histórico do Estado; Catedral de São Basílico; Mausoléu de Lênin<br>#Roteiro B | Região: Centro<br>Teatro Bolshoi; Monumento a Karl Marx; Rio Moscou<br>#Roteiro C | Região: Obras pela cidade<br>Metrô de Moscou; As Sete Irmãs; Moscow Leningradsky Railway Station  <br>
 
 </body>
 
 </html>`
 
+function GulliverTraveller(valorHtml){
 //O nome das cidades avaliadas.
 let htmlSplit = valorHtml;
 let cidadesAvaliadas = htmlSplit.split('*', 6);
@@ -66,3 +66,6 @@ alert("Encontrado na posição: " + findLosAngeles );
 //O nome dos pontos turísticos localizados no bairro Downtown na cidade de Las Vegas.
 let ptDowntown = valorHtml.slice(1078, 1143);
 alert("Pontos Turísticos do Centro de Las Vegas : " +ptDowntown);
+}
+
+GulliverTraveller(valorHtml);
